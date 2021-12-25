e64e4953790e75e0e677cbf2691b1de6fe861824
ColdForeign <tankon46555@gmail.com>
2021-12-25 18:22:07 +0200

    Migrating from MVC to Blazor. Removed unnecessary files

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,12 +1,6 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ElectronNET.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace WebApp
 {
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,15 +1,11 @@
+using ElectronNET.API;
+using ElectronNET.API.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ElectronNET.API;
-using ElectronNET.API.Entities;
+using WebApp.Data;
 
 namespace WebApp
 {
@@ -23,9 +19,12 @@ namespace WebApp
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,21 +37,18 @@ namespace WebApp
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
             });
+
 
             if (HybridSupport.IsElectronActive)
             {
@@ -63,8 +59,10 @@ namespace WebApp
         private async void CreateWindow()
         {
             var window = await Electron.WindowManager.CreateWindowAsync(
-                new BrowserWindowOptions { AutoHideMenuBar = true 
-            });
+                new BrowserWindowOptions
+                {
+                    AutoHideMenuBar = true
+                });
             window.OnClosed += () =>
             {
                 Electron.App.Quit();
