38b6a336c0abcf69c45d8b99d754754299043f1c
ColdForeign <tankon46555@gmail.com>
2023-08-07 12:01:23 +0300

    Initial code refactoring

--- src/FIP.App/FIP.App/Views/CreateCustomIconPage.xaml.cs
+++ src/FIP.App/FIP.App/Views/CreateCustomIconPage.xaml.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
 using Microsoft.Graphics.Canvas.Svg;
 using Microsoft.Graphics.Canvas.UI.Xaml;
 using Microsoft.UI.Xaml;
@@ -15,33 +12,19 @@ using FIP.Backend.Helpers;
 using CommunityToolkit.WinUI;
 using System.IO;
 using System.Text;
-using Microsoft.Graphics.Canvas;
-using Microsoft.Graphics.Canvas.Effects;
-using Microsoft.Graphics.Canvas.Text;
 using Microsoft.UI;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Windows.Storage.Pickers;
-using Windows.UI.Popups;
-using Microsoft.UI.Xaml.Media;
-using Windows.Storage.Streams;
 using Svg;
-using static System.Net.Mime.MediaTypeNames;
-using System.Drawing.Imaging;
 using FIP.App.Helpers;
 using System.Drawing.Drawing2D;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace FIP.App.Views
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Page for create custom icon
     /// </summary>
     public sealed partial class CreateCustomIconPage : Page
     {
-        CanvasSvgDocument svgDocument;
+        CanvasSvgDocument canvasSVG;
 
         Color defaultFolderColor = CommunityToolkit.WinUI.Helpers.ColorHelper.ToColor("#04805C");
 
@@ -73,13 +56,13 @@ namespace FIP.App.Views
 
         private void CanvasControl_Draw(CanvasControl sender, CanvasDrawEventArgs args)
         {
-            if (svgDocument == null)
+            if (canvasSVG == null)
             {
                 canvasControl.Invalidate();
                 return;
             }
 
-            args.DrawingSession.DrawSvg(svgDocument, sender.Size);
+            args.DrawingSession.DrawSvg(canvasSVG, sender.Size);
         }
 
         private async void mainColorPicker_ColorChanged(ColorPicker sender, ColorChangedEventArgs args)
@@ -99,38 +82,36 @@ namespace FIP.App.Views
 
             await Task.Run(async Task<bool> () =>
             {
-                if (svgDocument != null)
+                if (canvasSVG != null)
                 {
                     // Refill BackRect Gradient 
-                    CanvasSvgNamedElement backGradientFirstStop = svgDocument.FindElementById("BackGradientFirstStop");
+                    CanvasSvgNamedElement backGradientFirstStop = canvasSVG.FindElementById("BackGradientFirstStop");
                     backGradientFirstStop.SetStringAttribute("stop-color", fipColor.ToString(ColorOutputFormats.Hex));
 
                     backSecondColor = fipColor.ChangeSL(-0.13, -0.06);
-                    CanvasSvgNamedElement backGradientSecondStop = svgDocument.FindElementById("BackGradientSecondStop");
+                    CanvasSvgNamedElement backGradientSecondStop = canvasSVG.FindElementById("BackGradientSecondStop");
                     backGradientSecondStop.SetStringAttribute("stop-color", backSecondColor.ToString(ColorOutputFormats.Hex));
 
                     // Refill MiddleRect Gradient 
                     middleFirstColor = fipColor.ChangeSL(-0.38, 0.33);
-                    CanvasSvgNamedElement middleGradientFirstStop = svgDocument.FindElementById("MiddleGradientFirstStop");
+                    CanvasSvgNamedElement middleGradientFirstStop = canvasSVG.FindElementById("MiddleGradientFirstStop");
                     middleGradientFirstStop.SetStringAttribute("stop-color", middleFirstColor.ToString(ColorOutputFormats.Hex));
 
                     middleSecondColor = fipColor.ChangeSL(-0.2, 0.14);
-                    CanvasSvgNamedElement middleGradientSecondStop = svgDocument.FindElementById("MiddleGradientSecondStop");
+                    CanvasSvgNamedElement middleGradientSecondStop = canvasSVG.FindElementById("MiddleGradientSecondStop");
                     middleGradientSecondStop.SetStringAttribute("stop-color", middleSecondColor.ToString(ColorOutputFormats.Hex));
 
                     // Refill FrontRect Gradient 
                     frontFirstColor = fipColor.ChangeSL(-0.14, 0.18);
-                    CanvasSvgNamedElement frontGradientFirstStop = svgDocument.FindElementById("FrontGradientFirstStop");
+                    CanvasSvgNamedElement frontGradientFirstStop = canvasSVG.FindElementById("FrontGradientFirstStop");
                     frontGradientFirstStop.SetStringAttribute("stop-color", frontFirstColor.ToString(ColorOutputFormats.Hex));
 
                     frontSecondColor = fipColor.ChangeSL(0.06, 0.07);
-                    CanvasSvgNamedElement frontGradientSecondStop = svgDocument.FindElementById("FrontGradientSecondStop");
+                    CanvasSvgNamedElement frontGradientSecondStop = canvasSVG.FindElementById("FrontGradientSecondStop");
                     frontGradientSecondStop.SetStringAttribute("stop-color", frontSecondColor.ToString(ColorOutputFormats.Hex));
 
                     // Draw refilled svg image
                     canvasControl.Invalidate();
-
-                   
                 }
                 return await Task.FromResult(true);
             });
@@ -146,81 +127,41 @@ namespace FIP.App.Views
             FrontSecond.Text = frontSecondColor.ToString(ColorOutputFormats.HSL);
         }
 
+        // Saves created image
         private async void createButton_Click(object sender, RoutedEventArgs e)
         {
-            if (svgDocument == null)
+            if (canvasSVG == null)
                 return;
 
             var svgString = "";
             using (var stream = new MemoryStream())
             {
-                await svgDocument.SaveAsync(stream.AsRandomAccessStream());
+                await canvasSVG.SaveAsync(stream.AsRandomAccessStream());
 
                 stream.Position = 0;
                 var streamReader = new StreamReader(stream, Encoding.UTF8);
                 svgString = streamReader.ReadToEnd();
             }
 
-            //var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/win11-folder-green2.svg"));
+            SvgDocument newSVG = SvgDocument.FromSvg<SvgDocument>(svgString);
+            newSVG.ShapeRendering = SvgShapeRendering.Auto;
+            newSVG.Ppi = 72;
+            System.Drawing.Bitmap svgBitmap = newSVG.Draw(256, 256);
+            var svgBitmapGraphics = System.Drawing.Graphics.FromImage(svgBitmap);
+            svgBitmapGraphics.SmoothingMode = SmoothingMode.Default;
+            svgBitmapGraphics.DrawImage(svgBitmap, 0, 0, 256, 256);
 
-            SvgDocument svg = SvgDocument.FromSvg<SvgDocument>(svgString);
-            svg.ShapeRendering = SvgShapeRendering.Auto;
-            svg.Ppi = 272;
-
-            System.Drawing.Bitmap bitmap = svg.Draw(256, 256);
-            var g = System.Drawing.Graphics.FromImage(bitmap);
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
-            g.DrawImage(bitmap, 0, 0, 256, 256);
-
-            StorageFolder storageFolder = KnownFolders.SavedPictures;
-            StorageFile storageFile = await storageFolder.CreateFileAsync($"{NameBox.Text}.ico");
-            StorageFile storageFile33 = await storageFolder.CreateFileAsync($"{NameBox.Text}.png");
-            bitmap.Save(storageFile33.Path);
-
-            StorageFile storageFile2 = await storageFolder.CreateFileAsync($"{NameBox.Text}.svg");
-
-            await ImageHelper.SaveBitmapAsIconAsync(bitmap, storageFile.Path);
-            using (StreamWriter outputFile = new StreamWriter(storageFile2.Path))
-            {
-                outputFile.WriteLine(svgString);
-            }
-        }
-
-        CanvasVirtualBitmap virtualBitmap;
-
-        private async void OnSaveAsClicked(object sender, RoutedEventArgs e)
-        {
-            using (var device = CanvasDevice.GetSharedDevice())
-            using (var renderTarget = new CanvasRenderTarget(device, 512, 512, 96))
-            {
-                using (var ds = renderTarget.CreateDrawingSession())
-                {
-                    ds.DrawText("Win2D", 512 / 2, 512 / 2, Colors.White,
-                    new CanvasTextFormat()
-                    {
-                        VerticalAlignment = CanvasVerticalAlignment.Center,
-                        HorizontalAlignment = CanvasHorizontalAlignment.Center,
-                        FontFamily = "Comic Sans MS",
-                        FontSize = (float)(512 / 4)
-                    });
-                }
-                Random random = new Random();
-                StorageFolder storageFolder = KnownFolders.SavedPictures;
-                var file = await storageFolder.CreateFileAsync($"sample{random.Next()}.jpg", CreationCollisionOption.ReplaceExisting);
-
-                using (var fileStream = await file.OpenAsync(FileAccessMode.ReadWrite))
-                {
-                    await renderTarget.SaveAsync(fileStream, CanvasBitmapFileFormat.Png);
-                }
-            }
+            StorageFolder iconsFolder = KnownFolders.SavedPictures;
+            StorageFile newIconFile = await iconsFolder.CreateFileAsync($"{NameBox.Text}.ico");
+            await ImageHelper.SaveBitmapAsIconAsync(svgBitmap, newIconFile.Path);
         }
 
         private async void canvasControl_CreateResources(CanvasControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
         {
-            var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/win11-folder-green2.svg"));
-            using (var fileStream = await file.OpenReadAsync())
+            var svgFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/win11-folder-green2.svg"));
+            using (var fileStream = await svgFile.OpenReadAsync())
             {
-                svgDocument = await CanvasSvgDocument.LoadAsync(canvasControl, fileStream);
+                canvasSVG = await CanvasSvgDocument.LoadAsync(canvasControl, fileStream);
                 canvasControl.Invalidate();
             }
         }
