379a252921440711bda7977b28aaf91a2ddb8a3e
ColdForeign <tankon46555@gmail.com>
2023-08-09 11:52:42 +0300

    New models

---
+++ src/FIP.App/FIP.App/Models/Folder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIP.App.Models
+{
+    public class Folder : IEquatable<Folder>
+    {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// HEX formatted color.
+        /// </summary>
+        public string Color { get; set; }
+
+        public bool Equals(Folder other) =>
+            Name == other.Name &&
+            Color == other.Color;
+    }
+}
