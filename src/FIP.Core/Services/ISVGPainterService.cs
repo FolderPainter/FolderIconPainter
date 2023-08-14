eef5eed9b6f7ecde520c99f780b502fcbc6feedf
ColdForeign <tankon46555@gmail.com>
2023-08-14 12:33:09 +0300

    Rename project

---
+++ src/FIP.Core/Services/ISVGPainterService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Graphics.Canvas.Svg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIP.Backend.Services
+{
+    /// <summary>
+	/// Represents a service used for repaint svg gradients
+	/// </summary>
+    public interface ISVGPainterService
+    {
+        CanvasSvgDocument CanvasSVG
+
+    }
+}
