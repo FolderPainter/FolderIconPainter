6398fca77edaa3637b3a004060d5a565b9d560a6
ColdForeign <tankon46555@gmail.com>
2023-08-18 12:42:04 +0300

    Add Logger Util

---
+++ src/FIP.Shared/Utils/Logger/FileLoggerProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace FIP.Shared
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        private readonly string path;
+
+        public FileLoggerProvider(string path)
+        {
+            this.path = path;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            var logger = new FileLogger(path);
+            _ = Task.Run(() => logger.PurgeLogs(100));
+            return logger;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
