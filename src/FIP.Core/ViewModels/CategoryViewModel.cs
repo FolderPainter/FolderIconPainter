5ff0aa3b30b405111867988c4e34740b9490976f
ColdForeign <tankon46555@gmail.com>
2023-08-22 11:31:12 +0300

    Move and rename models

---
+++ src/FIP.Core/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+
+namespace FIP.Core.ViewModels
+{
+    [Serializable]
+    public class CategoryViewModel : ObservableObject
+    {
+        private string name;
+
+        public Guid Id { get; set; }
+
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
---
+++ src/FIP.Core/ViewModels/CustomIconViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+
+namespace FIP.Core.ViewModels
+{
+    [Serializable]
+    public class CustomIconViewModel : ObservableObject, IEquatable<CustomIconViewModel>
+    {
+        private string name;
+        private string infoTip;
+        private string color;
+
+        public Guid Id { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        public string InfoTip 
+        { 
+            get => infoTip; 
+            set => SetProperty(ref infoTip, value);
+        }
+
+        /// <summary>
+        /// HEX formatted color.
+        /// </summary>
+        public string Color 
+        { 
+            get => color; 
+            set => SetProperty(ref color, value); 
+        }
+
+        public bool Equals(CustomIconViewModel other) =>
+            Name == other.Name &&
+            Color == other.Color;
+    }
+}
