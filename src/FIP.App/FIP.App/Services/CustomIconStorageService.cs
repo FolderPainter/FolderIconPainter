84d7939db51cff10b22fc888b8857d072d44245c
ColdForeign <tankon46555@gmail.com>
2023-08-23 10:21:10 +0300

    Create CustomIconStorage service

---
+++ src/FIP.App/FIP.App/Services/CustomIconStorageService.cs
@@ -0,0 +1,68 @@
+﻿using FIP.App.Constants;
+using FIP.Core.Services;
+using FIP.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Storage;
+
+namespace FIP.App.Services
+{
+    public class CustomIconStorageService : BaseJSONStorageService, ICustomIconStorageService
+    {
+        private IEnumerable<CustomIconViewModel> _customIcons;
+
+        public event EventHandler OnCustomIconsUpdated;
+
+        public CustomIconStorageService()
+        {
+            Initialize(Path.Combine(ApplicationData.Current.LocalFolder.Path,
+                AppConstants.StorageSettings.StorageFolderName, AppConstants.StorageSettings.FolderIconsStorageFileName));
+
+            _customIcons = GetAllValues<CustomIconViewModel>();
+        }
+
+        public IEnumerable<CustomIconViewModel> CustomIcons
+        {
+            get => _customIcons;
+            set
+            {
+                _customIcons = value;
+                SetAllValues(value);
+                OnCustomIconsUpdated.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public CustomIconViewModel GetCustomIconById(Guid id)
+        {
+            return CustomIcons.SingleOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<CustomIconViewModel> GetCustomIconsByCategoryId(Guid categoryId)
+        {
+            return CustomIcons.Where(x => x.CategoryId == categoryId);
+        }
+
+        public void AddCustomIcon(CustomIconViewModel customIcon)
+        {
+            var customIcons = CustomIcons.ToList();
+            customIcons.Add(customIcon);
+            CustomIcons = customIcons;
+        }
+
+        public void DeleteCustomIconById(Guid id)
+        {
+            var customIcons = CustomIcons.ToList();
+            customIcons.RemoveAll(category => category.Id == id);
+            CustomIcons = customIcons;
+        }
+
+        public void DeleteCustomIconByCategoryId(Guid categoryId)
+        {
+            var customIcons = CustomIcons.ToList();
+            customIcons.RemoveAll(customIcon => customIcon.CategoryId == categoryId);
+            CustomIcons = customIcons;
+        }
+    }
+}
---
+++ src/FIP.Core/Services/ICustomIconStorageService.cs
@@ -0,0 +1,23 @@
+﻿using FIP.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+
+namespace FIP.Core.Services
+{
+    public interface ICustomIconStorageService
+    {
+        event EventHandler OnCustomIconsUpdated;
+
+        IEnumerable<CustomIconViewModel> CustomIcons { get; set; }
+
+        CustomIconViewModel GetCustomIconById(Guid id);
+
+        IEnumerable<CustomIconViewModel> GetCustomIconsByCategoryId(Guid categoryId);
+
+        void AddCustomIcon(CustomIconViewModel category);
+
+        void DeleteCustomIconById(Guid id);
+
+        void DeleteCustomIconByCategoryId(Guid categoryId);
+    }
+}
