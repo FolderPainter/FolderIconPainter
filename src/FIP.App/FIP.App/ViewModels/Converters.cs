3128ade45caaca7aea9b1c02a2c96d377f974961
ColdForeign <tankon46555@gmail.com>
2023-08-29 10:11:31 +0300

    Add converter

---
+++ src/FIP.App/FIP.App/ViewModels/Converters.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace FIP.App.ViewModels
+{
+    /// <summary>
+    /// Provides static methods for use in x:Bind function binding to convert bound values to the required value.
+    /// </summary>
+    public static class Converters
+    {
+        /// <summary>
+        /// Returns the reverse of the provided value.
+        /// </summary>
+        public static bool Not(bool value) => !value;
+
+        /// <summary>
+        /// Returns true if the specified value is not null; otherwise, returns false.
+        /// </summary>
+        public static bool IsNotNull(object value) => value != null;
+    }
+}
