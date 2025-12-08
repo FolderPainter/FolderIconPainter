2b7e5295d0cca328748cdcd5bcc00a389cd6d67c
ColdForeign <tankon46555@gmail.com>
2025-12-08 11:21:25 +0200

    Fix AllFolderIcons View Model for unpackaged build

--- src/FIP.App/FIP.App/ViewModels/AllFolderIconsViewModel.cs
+++ src/FIP.App/FIP.App/ViewModels/AllFolderIconsViewModel.cs
@@ -1,11 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
 using FIP.App.Constants;
+using FIP.App.Helpers;
 using FIP.Core.Models;
 using FIP.Core.Services;
 using FIP.Core.ViewModels;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
@@ -20,12 +20,23 @@ namespace FIP.App.ViewModels
 
         private ICustomIconStorageService CustomIconStorageService { get; } = Ioc.Default.GetRequiredService<ICustomIconStorageService>();
 
-        public IEnumerable<CategoryViewModel> Categories { get => CategoryStorageService.Categories.Select(c => new CategoryViewModel(c)).Append(new (new Category { Id = Guid.Empty })); }
+        public IEnumerable<CategoryViewModel> Categories { get => CategoryStorageService.Categories.Select(c => new CategoryViewModel(c)).Append(new(new Category { Id = Guid.Empty })); }
 
         private async Task<GroupInfoList> GetDefaultIconsAsync()
         {
-            Uri sourceUri = new Uri(AppConstants.AssetPaths.DefaultIconsJSON);
-            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(sourceUri);
+            StorageFile? file = null;
+            if (!NativeMethods.IsAppPackaged)
+            {
+                var sourcePath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, AppConstants.AssetPaths.DefaultIconsJSON));
+
+                file = await StorageFile.GetFileFromPathAsync(sourcePath);
+            }
+            else
+            {
+                Uri sourceUri = new Uri("ms-appx:///" + AppConstants.AssetPaths.DefaultIconsJSON);
+                file = await StorageFile.GetFileFromApplicationUriAsync(sourceUri);
+            }
+
             string jsonText = await FileIO.ReadTextAsync(file);
 
             var defaultCustomIcons = JsonSerializer.Deserialize<List<CustomIcon>>(jsonText, new JsonSerializerOptions
@@ -51,14 +62,13 @@ namespace FIP.App.ViewModels
 
             var query = from item in icons
                         group item by item.CategoryId into g
-                        select new GroupInfoList(g.Select(g => new DropZoneViewModel(new (g))))
+                        select new GroupInfoList(g.Select(g => new DropZoneViewModel(new(g))))
                         {
                             Key = g.Key,
                             Title = g.Key == Guid.Empty ? "No Category" : CategoryStorageService.GetCategoryById(g.Key).Name
                         };
             query = query.Prepend(await GetDefaultIconsAsync());
 
-            //return new ObservableCollection<GroupInfoList>(query.Prepend(GetDefaultIcons()));
             return new List<GroupInfoList>(query);
         }
     }
