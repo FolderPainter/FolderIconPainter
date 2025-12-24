3070f8ade5832d3d709ebc9c46c3635ee4f08801
ColdForeign <tankon46555@gmail.com>
2025-12-24 12:32:17 +0200

    Create settings helper

---
+++ src/FIP.App/FIP.App/Helpers/Settings/SettingsHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace FIP.App.Helpers.Settings;
+
+public partial class SettingsHelper : ObservableSettings
+{
+    private static readonly SettingsHelper instance = new(SettingsProviderFactory.CreateProvider());
+    public static SettingsHelper Current => instance;
+
+    private SettingsHelper(ISettingsProvider provider)
+        : base(provider)
+    {
+    }
+    public const int MaxRecentlyVisitedSamples = 7;
+
+    public ElementTheme SelectedAppTheme
+    {
+        get => GetOrCreateDefault<ElementTheme>(ElementTheme.Default);
+        set => Set(value);
+    }
+
+    public bool IsLeftMode
+    {
+        get => GetOrCreateDefault<bool>(true);
+        set => Set(value);
+    }
+
+    public bool IsShowCopyLinkTeachingTip
+    {
+        get => GetOrCreateDefault<bool>(true);
+        set => Set(value);
+    }
+
+    public bool IsFirstRun
+    {
+        get => GetOrCreateDefault<bool>(true);
+        set => Set(value);
+    }
+}
